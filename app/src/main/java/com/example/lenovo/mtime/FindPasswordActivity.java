c24671881c51429cf57fdd2ef4c5fee6757b6f53
huaidanbushidan <43977595+huaidanbushidan@users.noreply.github.com>
2019-04-09 16:11:10 +0800

    找回密码更新

--- app/src/main/java/com/example/lenovo/mtime/FindPasswordActivity.java
+++ app/src/main/java/com/example/lenovo/mtime/FindPasswordActivity.java
@@ -3,11 +3,28 @@ package com.example.lenovo.mtime;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FindPasswordActivity extends AppCompatActivity {
+    EditText User_id;
+    EditText newPassword;
+    EditText repeatPassword;
+    EditText Code;
+    Button btn_sendCode;
+    Button btn_Reset;
+    Button btn_Cancel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_password);
+        User_id = findViewById(R.id.et_UserId);
+        newPassword = findViewById(R.id.et_newPassword);
+        repeatPassword = findViewById(R.id.et_repeatPassword);
+        Code = findViewById(R.id.et_code);
+        btn_sendCode = findViewById(R.id.send_code);
+        btn_Reset = findViewById(R.id.btn_reset);
+        btn_Cancel = findViewById(R.id.btn_out);
     }
 }
